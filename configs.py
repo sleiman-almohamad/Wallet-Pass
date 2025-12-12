0cd559ceb67265a9fe44c822a0be82fce7963e07
sleiman-almohamad <sleiman.almohamad@gmail.com>
2025-12-12 10:36:19 +0100

    *.json

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@
 ISSUER_ID = "3388000000023033675"
 
 # Default key file name
-KEY_FILE_PATH = "wallet-pass-476608-cb41e3d2ff5c.json"
+KEY_FILE_PATH = "*.json"
 
 # Default scopes
 SCOPES = ['https://www.googleapis.com/auth/wallet_object.issuer']
